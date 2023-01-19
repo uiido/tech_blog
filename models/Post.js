b1350aa00555bc6bafb2c9e4df8da8b91e55d773
uiido <uiido@yahoo.com>
2023-01-19 00:13:07 -0500

    Filled in post model

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+// Requires
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+// Superclass
+class Post extends Model { }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+// Exports
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,7 @@
+// Requires
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
+
+// Module exports
+module.exports = { User, Post, Comment };
