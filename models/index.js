820e0e4c732333da21a12c359a730e5dae01f7d1
uiido <uiido@yahoo.com>
2023-01-19 00:24:56 -0500

    Filled in model index for user

--- models/index.js
+++ models/index.js
@@ -3,5 +3,14 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// Throughs
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
 // Module exports
 module.exports = { User, Post, Comment };
