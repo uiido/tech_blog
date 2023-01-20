e21443944500e52f96e70bf6929a953a4c1c4472
uiido <uiido@yahoo.com>
2023-01-19 19:26:24 -0500

    Created test post in post seeds

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,5 +1,13 @@
 const { Post } = require('../models');
 
+const postData = [
+    {
+        title: 'This is a new post!',
+        content: 'This is the content of a new post!',
+        user_id: 1
+    }
+];
+
 const postSeeds = () => Post.bulkCreate(postData);
 
 module.exports = postSeeds;
