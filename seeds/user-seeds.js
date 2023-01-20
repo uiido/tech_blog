460770168e084034ee6a1d4ddc5a9303696623be
uiido <uiido@yahoo.com>
2023-01-19 19:22:05 -0500

    Inserted a test user into seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,5 +1,12 @@
 const { User } = require('../models/');
 
+const userData = [
+    {
+        username: 'testuser',
+        password: 'password123'
+    }
+];
+
 const userSeeds = () => bulkCreate(userData);
 
 module.exports = userSeeds;
