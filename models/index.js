2034fbdd0600d8c922feadbe9d38773158fa952e
uiido <uiido@yahoo.com>
2023-01-19 20:54:17 -0500

    Generating db still not working properly - placing it aside for now

--- models/index.js
+++ models/index.js
@@ -1,11 +1,20 @@
-// Requires
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-// Throughs
 User.hasMany(Post, {
     foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
 });
 
 User.hasMany(Comment, {
@@ -14,19 +23,5 @@ User.hasMany(Comment, {
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
-});
-
-Post.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-// Module exports
+})
 module.exports = { User, Post, Comment };
