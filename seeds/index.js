82720de528b8d15be21e3ce848d1607275894bb9
uiido <uiido@yahoo.com>
2023-01-19 19:40:40 -0500

    Filled in seed index with post and user seeds

--- seeds/index.js
+++ seeds/index.js
@@ -2,3 +2,11 @@ const userSeeds = require('./user-seeds.js');
 const postSeeds = require('./post-seeds.js');
 
 const sequelize = require('../config/connection.js');
+
+const seedDb = async () => {
+    await sequelize.sync({ force: true });
+    await userSeeds();
+    await postSeeds();
+};
+
+seedDb();
