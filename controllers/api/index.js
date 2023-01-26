5b1a387e039d7bc299764b59aa8bd989714b5a59
uiido <uiido@yahoo.com>
2023-01-26 01:08:29 -0500

    Trying to fill in posts to site ... nodemon crashes ....

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes.js');
+const { route } = require('./user-routes.js');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('comment', commentRoutes);
 
 module.exports = router;
