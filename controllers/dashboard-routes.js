a7256623288fad394cf6729bfae2a4181efd491c
uiido <uiido@yahoo.com>
2023-01-27 10:29:58 -0500

    Fixed error that occurs when nodemon always launches but allows it to run anyway - dashboard page now loads (instead of saying it doesn't exist) but only returns json. Still troubleshooting posts.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,4 +3,75 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models/');
 const withAuth = require('../utils/auth');
 
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content'
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'post_id', 'user_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id',
+            'title',
+            'content'
+        ],
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'post_id', 'user_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'Sorry, no post with this ID were found!' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', { post, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
 module.exports = router;
